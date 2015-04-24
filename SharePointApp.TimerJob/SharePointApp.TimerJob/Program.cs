c231a784976ae2f12d143e3d4b593b81dc683cf2
RARomano <rodrigo@rodrigoromano.net>
2015-04-23 22:54:32 -0300

    added code

--- SharePointApp.TimerJob/SharePointApp.TimerJob/Program.cs
+++ SharePointApp.TimerJob/SharePointApp.TimerJob/Program.cs
@@ -10,6 +10,28 @@ namespace SharePointApp.TimerJob
     {
         static void Main(string[] args)
         {
+            string siteUrl = "http://localhost";
+            Uri uri = new Uri(siteUrl);
+
+            string realm = TokenHelper.GetRealmFromTargetUrl(uri);
+
+            //Get the access token for the URL.  
+            //   Requires this app to be registered with the tenant
+            string accessToken = TokenHelper.GetAppOnlyAccessToken(
+                TokenHelper.SharePointPrincipal,
+                uri.Authority, realm).AccessToken;
+
+            using (var ctx = TokenHelper.GetClientContextWithAccessToken(uri.ToString(), accessToken))
+            {
+                /// codigo vai aqui
+                ctx.Load(ctx.Web);
+                var webTitle = ctx.Web.Title;
+
+                ctx.ExecuteQuery();
+
+                Console.Write(webTitle);
+
+            }
         }
     }
 }
